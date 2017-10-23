1d0c924fcd7171163358448dcfb0126bea4bb37c
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2017-10-23 07:19:13 -0400

    displayalert

---
+++ ReadDataFromJsonAndListViewSorting-5-4/ReadDataFromJson/SecondPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ReadDataFromJson
+{
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage(MyJsseResponse.RootObjectJSSE selecteJSSEObject)
+		{
+			InitializeComponent();
+			MessagingCenter.Subscribe<SecondPageViewModel>(this, "Hi", (sender) =>
+			{
+				// do something whenever the "Hi" message is sent
+				DisplayAlert("hiii", "data stored", "ok");
+			});
+			this.BindingContext = new SecondPageViewModel(selecteJSSEObject);
+		}
+	}
+}
---
+++ ReadDataFromJsonAndListViewSorting-5-4/ReadDataFromJson/SecondPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ReadDataFromJson
+{
+	public class SecondPageViewModel : ViewModelBase
+	{
+		public MyJsseResponse.RootObjectJSSE _selecteJSSEObject;
+
+		Command _savecommand;
+		public Command SaveChanges => _savecommand ?? (_savecommand = new Command(ExecuteSaveCommand));
+
+        private  void ExecuteSaveCommand()
+		{
+			//await this._messageservice.ShowAsync("you saved data");
+
+			 MessagingCenter.Send<SecondPageViewModel>(this, "Hi");
+            App.Current.MainPage.Navigation.PushAsync(new ReadDataFromJsonPage());
+			//inobj.FirstLabel = firstLabel;
+			//inobj.SecondLabel = secondLabel;
+			//inobj.SelectedDate = selectedDate;
+			//inobj.SelectedPickerTitle = selectedPickerTitle;
+		}
+		public SecondPageViewModel(MyJsseResponse.RootObjectJSSE selecteJSSEObject)
+		{
+			_selecteJSSEObject = selecteJSSEObject;
+			firstLabel = selecteJSSEObject.Observer.FullName;
+			SecondLabel = selecteJSSEObject.JSSEEnteredBy;
+		}
+
+		string firstLabel;
+		public string FirstLabel
+		{
+			get { return firstLabel; }
+			set
+			{
+				if (firstLabel != value)
+				{
+					firstLabel = value;
+					OnPropertyChanged("FirstLabel");
+				}
+			}
+		}
+
+		string secondLabel;
+		public string SecondLabel
+		{
+			get { return secondLabel; }
+			set
+			{
+				if (secondLabel != value)
+				{
+					secondLabel = value;
+					OnPropertyChanged("SecondLabel");
+				}
+			}
+		}
+	}
+}
